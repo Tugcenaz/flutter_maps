e1fe32d48492cdd55e215260e27a22c35d1b8767
Tugcenaz <nazlioglutugce@gmail.com>
2023-09-04 00:20:42 +0300

    galeriden fotoğraf seçilebilir kameradan fotoğraf çekilebilir

--- lib/app/controller/image_controller.dart
+++ lib/app/controller/image_controller.dart
@@ -3,14 +3,14 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 class ImageController extends GetxController{
-  Rx<File>? image = File('').obs;
+  Rx<File>? imageFile = File('').obs;
 
   Future pickImageFromGallery() async {
     try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      XFile?  image = await ImagePicker().pickImage(source: ImageSource.gallery);
       if (image == null) return;
-      final imageTemp = File(image.path);
-      this.image?.value = imageTemp;
+      File imageTemp = File(image.path);
+      imageFile?.value = imageTemp;
     } catch (e) {
       debugPrint('failed to pick image from gallery: $e');
     }
@@ -21,7 +21,7 @@ class ImageController extends GetxController{
       final image = await ImagePicker().pickImage(source: ImageSource.camera);
       if (image == null) return;
       final imageTemp = File(image.path);
-      this.image?.value = imageTemp;
+      imageFile?.value = imageTemp;
     } catch (e) {
       debugPrint('failed to pick image from camera : $e');
     }
